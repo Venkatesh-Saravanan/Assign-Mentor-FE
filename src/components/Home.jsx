5e65303dfa21df57587cf16df7aa996d1c3279e1
Venkatesh-Saravanan <ssvenkatesh1920@gmail.com>
2024-09-27 12:00:42 +0530

    initial commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,16 @@
+
+import Topbar from "./Topbar";
+
+function Home() {
+  return (
+    <>
+      <Topbar />
+      <div className="image ms-4 d-flex justify-content-center">
+        <img src="https://cdn.pixabay.com/photo/2020/10/05/06/21/tablet-5628298_1280.jpg" alt="home-image"/>
+      </div>
+    </>
+  );
+}
+
+export default Home;
+
